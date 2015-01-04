ff772f1d407cc0fc04774b635e7519585a07a67e
sancheeaman <sk11juit@gmail.com>
2015-01-05 00:33:51 +0530

    Added update feature

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
 		@article = Article.new		
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+
 	def create
 		@article = Article.new(article_params)	
 		if @article.save
@@ -20,6 +25,15 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
 	private
 		def article_params
 			params.require(:article).permit(:title, :text)
